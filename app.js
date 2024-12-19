c33f3688ee0689902542e4f2d0b5083873d0c98a
KLNCL <klncl@gmail.com>
2024-12-19 12:17:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// const express = require('express');
+// const app = express();
+// const controller = require(',/controller');
+
+// // app.use(
+// //     express.urlencoded({
+// //         extended: true,
+// //     })
+// // );
+
+// //need to get controller's data its for the rest apis
+
+// app.get('/users', (req, res) => {
+//     controller.getUsers((req, res)=> {
+//         res.send();
+//     });
+// });
+
+// app.post('/createusers', (req, res) => {
+//     controller.addUser(req.body, (callback) => {
+//         res.send(callback);
+//     });
+// });
+
+// app.post('/updateuser', (req, res) => {
+//     controller.updateUser(req.body, (callback) => {
+//         res.send(callback);
+//     });
+// })
+
+
+// app.post('/deleteuser', (req, res) => {
+//     controller.deleteUser(req.body, (callback) => {
+//         res.send(callback);
+//     });
+// })
+
+// app.use(express.json());
+// module.exports = app;
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const userRouter = require('./Routes/Router')
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+app.use(express.json()); // Parse JSON bodies
+
+app.use(
+    express.urlencoded({
+         extended: true,
+     })
+ );
+
+app.use('/api', userRouter);
+
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection Error"));
+
+db.once("open", () => {
+    console.log("MongoDB Connected");
+})
+
+//error handle
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
