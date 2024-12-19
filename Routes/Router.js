eb13ddc310e28c924952b94d6221ba9a33b84923
KLNCL <klncl@gmail.com>
2024-12-19 12:27:06 +0530

    Add routes

---
+++ Routes/Router.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const userController = require('../controller/userController');
+const authController = require('../controller/authController');
+const productConrtoller = require('../controller/productController');
+const orderController = require('../controller/orderController');
+
+
+//user routs
+router.get('/users', userController.getUsers);
+router.get('/user/:userID', userController.findUser);
+router.post('/createusers', userController.addUser);
+router.post('/updateuser/:userID', userController.updateUser);
+router.delete('/deleteuser/:userID', userController.deleteUser);
+
+
+//loging routs
+router.post('/login', authController.signin);
+
+
+//product routs
+router.get('/product', productConrtoller.getProducts);
+router.get('/products/:productID', productConrtoller.findProduct);
+router.get('/product/user', productConrtoller.findProductUid);
+router.post('/createproduct', productConrtoller.addProduct);
+router.post('/updateproduct/:productID', productConrtoller.updateProdut);
+router.delete('/deleteproduct/:productID', productConrtoller.deleteProduct);
+
+
+
+//order routs
+router.get('/getorder',orderController.getOrders);
+router.post('/createorder', orderController.addOrder);
+router.get('/orders/sellerid', orderController.findOrderSid);
+router.get('/order/userid', orderController.findOrderUid);
+router.post('/orderupdate/:orderID', orderController.updateOrder);
+router.delete('/deleteorder/:orderID', orderController.deleteOrder);
+
+
+module.exports = router;
+
