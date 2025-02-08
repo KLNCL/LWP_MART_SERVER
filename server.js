66ac84ca0eb4969827e1ac1261c2400803a4bb4f
KLNCL <klncl@gmail.com>
2025-02-08 12:08:15 +0530

    update socket.oi

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const userRouter = require('./Routes/Router')
-const http = require("http");
-const { Server } = require("socket.io");
+const http = require('http');
+const socketIo = require('socket.io');
 
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
 
 // Middleware
 app.use(cors());
@@ -52,33 +53,19 @@ app.use((err, req, res, next) => {
 });
 
 // WebSocket setup for real-time messaging
-const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5000", // Adjust based on your frontend URL
-      methods: ["GET", "POST"],
-    },
-  });
-  
-  io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-  
-    socket.on("sendMessage", async ({ sender, receiver, message }) => {
-      try {
-        const Message = require("./models/Message");
-        const newMessage = new Message({ sender_id: sender, receiver_id: receiver, message });
-        await newMessage.save();
-  
-        io.to(receiver).emit("receiveMessage", newMessage);
-      } catch (error) {
-        console.error("Error sending message:", error.message);
-      }
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
-    });
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('sendMessage', (data) => {
+    io.emit('receiveMessage', data);
   });
 
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
 
 // Start Server
 const PORT = process.env.PORT || 5000;
